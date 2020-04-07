3390e1c2a0f17340b224f2ca801bbdf1cff76c76
ISHaNT-CHaHaR <56401920+ISHaNT-CHaHaR@users.noreply.github.com>
2020-04-08 01:42:04 +0530

    did some good exercises.
    wait for more...

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();// app have all the functionality of express now.
+app.listen(8000, () => {
+   console.log('listening at 8000');
+});
+
+app.use(express.static('public')); // anything in this folder in publicaly acceptable by url/
+
